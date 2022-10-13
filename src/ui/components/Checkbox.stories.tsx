8377acce4869b69bcb179cba2be741b1b448b0c4
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-10-13 20:39:47 -0300

    feat: add Checkbox component with storybook docs & add radix-checkbox

---
+++ src/ui/components/Checkbox.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Checkbox } from '@/ui/components/Checkbox'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+} as Meta
+
+export const Default: StoryObj = {
+  args: {},
+}
