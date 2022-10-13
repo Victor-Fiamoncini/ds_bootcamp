4187f3a0391920103538fe755feb85b467280c5d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-10-13 08:08:10 -0300

    chore: add base node/react/storybook setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'eslint-plugin-import-helpers', 'prettier'],
+  rules: {
+    indent: ['error', 2, {
+      SwitchCase: 1
+    }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'import-helpers/order-imports': ['warn', {
+      newlinesBetween: 'always',
+      groups: ['/^react/', 'module', '/^@/main/', '/^@/test/', '/^@/ui/', ['parent', 'sibling', 'index']],
+      alphabetize: {
+        order: 'asc',
+        ignoreCase: true
+      }
+    }]
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@/main', replacement: resolve(__dirname, 'src/main') },
+      { find: '@/ui', replacement: resolve(__dirname, 'src/ui') },
+    ],
+  },
+  server: {
+    host: true,
+    port: 5000,
+  },
 })
