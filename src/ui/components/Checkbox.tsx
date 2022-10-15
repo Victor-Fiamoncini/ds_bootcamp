fb8e3b9418fcb02ecaee949447d063f28849d1b0
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-10-15 16:49:14 -0300

    refactor: add Button and Checkbox base html props

--- src/ui/components/Checkbox.tsx
+++ src/ui/components/Checkbox.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 
-export const Checkbox: React.FC = () => (
-  <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+export type Props = CheckboxPrimitive.CheckboxProps
+
+export const Checkbox: React.FC<Props> = ({ ...props }) => (
+  <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
     <CheckboxPrimitive.Indicator asChild>
       <Check className="h-5 w-5 text-cyan-500" weight="bold" />
     </CheckboxPrimitive.Indicator>
