57d127f1771c9277c7afcb8fa0775101378f1f8b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-10-15 13:18:42 -0300

    chore: add storybook/storybook-deployer dep & update gitignore and eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,32 +3,45 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true
+    jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint', 'eslint-plugin-import-helpers', 'prettier'],
   rules: {
-    indent: ['error', 2, {
-      SwitchCase: 1
-    }],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
-    'import-helpers/order-imports': ['warn', {
-      newlinesBetween: 'always',
-      groups: ['/^react/', 'module', '/^@/main/', '/^@/test/', '/^@/ui/', ['parent', 'sibling', 'index']],
-      alphabetize: {
-        order: 'asc',
-        ignoreCase: true
-      }
-    }]
-  }
-};
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['/^react/', 'module', '/^@/main/', '/^@/test/', '/^@/ui/', ['parent', 'sibling', 'index']],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
+}
