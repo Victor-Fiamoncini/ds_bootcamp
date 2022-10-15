a0e1e6cab9649cb6dbf555d960d9eb4e9eb4ed2f
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-10-15 13:55:37 -0300

    ci: publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -10,7 +10,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
-  async viteFinal(config) {
+  async viteFinal(config, { configType }) {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ds_bootcamp/'
+    }
+
     return {
       ...config,
       resolve: {
